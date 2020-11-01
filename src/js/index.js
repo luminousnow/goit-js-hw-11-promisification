928b5e9d6bd086e33e87d7f2b3ee1f47cbf77915
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-01 18:35:06 +0200

    Додає розвязання Завдання 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
+import { resolve } from 'path';
+import { reject } from 'q';
 import '../scss/style.scss';
+
+// Promise Задание 1
+const delay = ms => {
+  const DELAY = ms;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(DELAY);
+      reject();
+    }, DELAY);
+
+    return resolve;
+  });
+
+  return promise;
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(logger);
+delay(1000).then(logger);
+delay(1500).then(logger);
+
+// Promise Задание 2
