3a73825bb17ad66841b3560fd7e8286bd03ae598
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-02 03:06:29 +0200

    Updates

--- js.d818e0ef.js
+++ js.d818e0ef.js
@@ -2911,6 +2911,9 @@ var _path = require("path");
 var _q = require("q");
 
 require("../scss/style.scss");
+
+var p = new Promise(function (resolve, reject) {});
+console.log(p);
 },{"path":"node_modules/path-browserify/index.js","q":"node_modules/q/q.js","../scss/style.scss":"src/scss/style.scss"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -2939,7 +2942,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53692" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50906" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
